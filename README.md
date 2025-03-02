039ece4bd145f4b66f6f09e9ed8396911037a3f4
Maurezlg7 <gomezmauroleonel7@gmail.com>
2025-03-01 22:31:12 -0300

    Reinicio del repositorio

---
+++ README.md
@@ -0,0 +1,8 @@
+# PORTOFOLIO React + TSW
+
+Depencias instaladas:
+   **npm install**
+   **npm install react-router-dom**
+   **npm install gh-pages --save-dev**
+   **npm install bootstrap**
+   **npm install react-bootstrap bootstrap**
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,53 @@
+import { Link, Outlet } from 'react-router-dom';
+import '../assets/css/nav.css';
+
+export default function Navbar() {
+    return (
+        <>
+            <header>
+                <nav className="navbar navbar-expand-lg navbar-dark">
+                    <div className="container-fluid d-flex justify-content-between">
+                        <Link className='navbar-brand' to="/maurezlg7-portfolio/">
+                            <ul className="navbar-nav ms-auto d-flex align-items-center container_home">
+                                <li>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-circle-fill" viewBox="0 0 16 16">
+                                        <circle cx="8" cy="8" r="8" />
+                                    </svg>
+                                </li>
+                                <li>
+                                    <h3>Mauro L. Gomez</h3>
+                                </li>
+                                <li className='container-link-home'>
+                                    <span className='info'>Desarrollador de Software @ UPATECO</span>
+                                </li>
+                            </ul>
+                        </Link>
+
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav ms-auto">
+                                <li className="nav-item d-flex align-items-center justify-content-center mx-2">
+                                    <Link className='nav-link active' to={'CV'}>Curriculum Vitae</Link>
+                                </li>
+                                <li className="nav-item d-flex align-items-center justify-content-center mx-2">
+                                    <Link className='nav-link active' to={'Proyectos'}>Proyectos</Link>
+                                </li>
+                                <li className="nav-item d-flex align-items-center justify-content-center mx-2">
+                                    <Link className='nav-link active' to={'Habilidades'}>Habilidades</Link>
+                                </li>
+                                <li className="nav-item d-flex align-items-center justify-content-center mx-2">
+                                    <Link className='nav-link active' to={'Contacto'}>Contacto</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+            <Outlet />
+        </>
+    );
+}
